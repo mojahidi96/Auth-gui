fe7a93025322b511a4aef87e19c7ce236777e0ef
Mojahid Islam <Mojahid.Islam@synchronoss.com>
2020-10-30 22:33:48 +0530

    added header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationComponent } from './notification/notification.component';
 import { SpinnerComponent } from './spinner-overlay/spinner-overlay.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [NotificationComponent, SpinnerComponent],
+  declarations: [NotificationComponent, SpinnerComponent, HeaderComponent],
   imports: [
     CommonModule
   ],
-  exports: [NotificationComponent, SpinnerComponent]
+  exports: [NotificationComponent, SpinnerComponent, HeaderComponent]
 })
 export class SharedModule { }
