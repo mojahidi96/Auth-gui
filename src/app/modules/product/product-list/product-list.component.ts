eb089d69b33b544850b80d045bd8e14025e0ab51
Mojahid Islam <Mojahid.Islam@synchronoss.com>
2020-11-04 13:03:14 +0530

    mocked product list

--- src/app/modules/product/product-list/product-list.component.ts
+++ src/app/modules/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import product from '../../../../mock-json/product.json'
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   constructor() { }
-
+  products
   ngOnInit(): void {
+    this.products = this.generateRows(product.ProductCatalogResponse.ProductList.product);
+    console.log(product.ProductCatalogResponse)
+  }
+
+  generateRows(list, numberOfColumns = 4) {
+    let tempArray = [];
+    for (let index = 0; index < list.length; index += 4) {
+      tempArray.push(list.slice(index, index + numberOfColumns));
+    }
+    return tempArray
   }
 
 }
